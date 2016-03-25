7b8a6ea1f7896cf9f28b207f50455bbb2ca79e38
Alberto97 <albertop2197@gmail.com>
2016-03-25 15:43:52 +0100

    Revert "Add contact query for FDN and SDN"
    This reverts commit 0490097ed300bb199064b59887a4f67a912e2a95.

--- src/com/android/incallui/CallerInfo.java
+++ src/com/android/incallui/CallerInfo.java
@@ -88,9 +88,6 @@ public class CallerInfo {
     public int namePresentation;
     public boolean contactExists;
 
-    public boolean callerInfoContactSearch;
-    public boolean isSdnSearch;
-
     public String phoneLabel;
     /* Split up the phoneLabel into number type and label name */
     public int    numberType;
